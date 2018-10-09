368ea5f7518f885b1099f98520a992f27786a4a7
rakesh333patra <43970277+rakesh333patra@users.noreply.github.com>
2018-10-09 18:51:16 +0530

    Add files via upload

--- apiProgQ5.py
+++ apiProgQ5.py
@@ -1,17 +1,58 @@
+"""As the given api didnt work, I have performed this using a restful localhost"""
 import requests
 import traceback
 import json
 
-def postMethod():
-    
-    url = 'https://dev.rackspace.example.com/widgets'
-    header={"content-type":"application/json","Authorization":"5675309"}
-    payload = {"widget_name": "Test Widget","description": "Description"}
-    
-    r = requests.post(url, data=payload)
-    print r.text
-    print r.status_code
-    
+def postMethod(url):
+
+    try:
+        
+        header={"content-type":"application/json"}
+        payload = {"widget_name": "Test Widget","description": "Description"}
+        
+        r = requests.post(url, data=json.dumps(payload), headers=header)
+        print r.text
+        print r.status_code
+    except Exception,e:
+        print e
+        
+def getMethod(url):
+    try:
+        val=requests.get(url)
+        
+        data = r.json() 
+        print data
+    except Exception,e:
+        print e
+        
+def putMethod(url):
+
+    try:
+        
+        header={"content-type":"application/json"}
+        payload = {"widget_name": "Test Widget New"}
+        
+        r = requests.put(url, data=json.dumps(payload), headers=header)
+        print r.text
+        print r.status_code
+    except Exception,e:
+        print e
+        
+def deleteMethod(url):
+    try:
+        payload = {'widget_name':'Test Widget New'}
+        headers = {'content-type': 'application/json'}
+        
+        r = requests.delete(url, data=json.dumps(payload), headers=header)        
+        print r.text
+        print r.status_code
+    except Exception,e:
+        print e
     
 if __name__=="__main__":
-    postMethod()
+    testUrl='https://localhost/widgets.php'
+    """Choose your operation by commenting out rest"""
+    postMethod(testUrl)
+    getMethod(testUrl)
+    putMethod(testUrl)
+    deleteMethod(testUrl)
